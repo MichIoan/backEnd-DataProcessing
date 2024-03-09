c3b0009b5123520f0e1a63b464fecbba557eb426
MichIoan <ioan.mich@student.nhlstenden.com>
2024-03-09 19:15:35 +0100

    Modified tests so no data will be kept in the database after the tests

--- __tests__/AUTH/registerTest.js
+++ __tests__/AUTH/registerTest.js
@@ -1,21 +1,24 @@
 const axios = require('axios');
-const url = 'http://localhost:8081';
-const email = 'test@user.com';
-const password = 'Password1';
+const pool = require('../db_conn');
 const userDetails = {
-    email,
-    password
+    email: "test@user.com",
+    password: "Password1"
 };
-const registerURL = `${url}/auth/register`;
+const registerURL = `http://localhost:8081/auth/register`;
+let usersToDelete;
 
 describe('POST /register', () => {
 
-    it('Invalid email format', async () => {
+    it("Delete the user before tests", async () => {
+        await pool.query(`DELETE FROM "Users" WHERE "Users"."email"=$1`, [userDetails.email]);
+    })
+
+    it('Register user with invalid email format should return 400', async () => {
         userDetails.email = 'asdasd';
         try {
-            const response = await axios.post(registerURL, userDetails);
+            await axios.post(registerURL, userDetails);
         } catch (error) {
-            expect(error.response.status).toBe(401);
+            expect(error.response.status).toBe(400);
             expect(error.response.data).toEqual({
                 "error": "Invalid email format!"
             });
@@ -23,87 +26,61 @@ describe('POST /register', () => {
     });
 
     it("Register new user should return 201", async () => {
-        // Add logic here to attempt registering a new user
-        const uniqueId = Date.now();
-        const email = `new${uniqueId}@user.com`;
+        const email = `existing@user.com`;
         userDetails.email = email;
-        try {
-            const response = await axios.post(registerURL, userDetails);
-        } catch (error) {
-            expect(error.response.status).toBe(201);
-            expect(error.response.data).toEqual({
-                "error": "New user registered"
-            });
-        }
+        usersToDelete = email;
+        const response = await axios.post(registerURL, userDetails);
+        expect(response.status).toBe(201);
+        expect(response.data).toEqual({
+            "message": "User registered successfully. Activate account from email!"
+        });
     });
 
-
-    it("Register existing user should return 409", async () => {
-        // Add logic here to attempt registering an existing user
+    it("Register existing user should return 400", async () => {
         userDetails.email = 'existing@user.com';
         try {
-            const response = await axios.post(registerURL, userDetails);
+            await axios.post(registerURL, userDetails);
         } catch (error) {
-            expect(error.response.status).toBe(409);
+            expect(error.response.status).toBe(400);
             expect(error.response.data).toEqual({
                 "error": "User with this email already exists"
             });
         }
     });
 
-    it("Register user with invalid email format should return 401", async () => {
-        // Add logic here to attempt registering a new user with invalid email format
-        userDetails.email = 'invalidformat'
-        try {
-            const response = await axios.post(registerURL, userDetails);
-        } catch (error) {
-            expect(error.response.status).toBe(401);
-            expect(error.response.data).toEqual({
-                "error": "Invalid email format!"
-            });
-        }
-
+    it("Delete users created in the last test", async () => {
+        await pool.query(`DELETE FROM "Users" WHERE "Users"."email"=$1`, [usersToDelete]);
     });
 
-    it("Register user with invalid password should return 401", async () => {
+    it("Register user with invalid password should return 400", async () => {
         // Add logic here to attempt registering a new user with invalid password
         userDetails.email = 'correct@email.com'
         userDetails.password = 'jvho'
         try {
-            const response = await axios.post(registerURL, userDetails);
+            await axios.post(registerURL, userDetails);
         } catch (error) {
-            expect(error.response.status).toBe(401);
+            expect(error.response.status).toBe(400);
             expect(error.response.data).toEqual({
                 "error": 'Password must contain at least 1 capital, a number and 6 characters'
             });
         }
-
     });
 
-    it('Registering with empty fields should return 401', async () => {
-        userDetails.email = ""
-
-        try {
-            const response = await axios.post(registerURL, userDetails);
-        } catch (error) {
-            expect(error.response.status).toBe(401);
-            expect(error.response.data).toEqual({
-                "error": 'Invalid email format!'
-            });
-        }
-    });
-
-    it('Registering with empty fields should return 401', async () => { 
+    it('Registering with empty fields should return 400', async () => {
         userDetails.email = "valid@email.com"
         userDetails.password = ""
         try {
-            const response = await axios.post(registerURL, userDetails);
+            await axios.post(registerURL, userDetails);
         } catch (error) {
-            expect(error.response.status).toBe(401);
+            expect(error.response.status).toBe(400);
             expect(error.response.data).toEqual({
                 "error": 'Password must contain at least 1 capital, a number and 6 characters'
             });
         }
     });
 
+    it("Delete user and close db conn after all tests ran", async () => {
+        await pool.query(`DELETE FROM "Users" WHERE "Users"."email"=$1`, [userDetails.email])
+        pool.end();
+    });
 });
