92c1a15cf5baef241483f6a9dbad688cc2b904af
Hakan Ege Özerol <hakan.ege.ozerol@student.nhlstenden.com>
2024-03-10 02:25:05 +0100

    changed tests for profile edit and creation --FIX

--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -8,9 +8,7 @@ const getSubscriptionInfo = async (req, res) => {
     const userId = req.params.userId;
 
     if (!userId) {
-      response(req, res, 400, {
-        message: "Please provide the user id as a parameter"
-      });
+      response(req, res, 400, { message: "Please provide the user id as a parameter" });
       return;
     }
 
@@ -21,30 +19,24 @@ const getSubscriptionInfo = async (req, res) => {
     });
 
     if (!subscription) {
-      response(req, res, 400, {
-        error: "No subscription was found for this user."
-      });
+      response(req, res, 400, { error: "No subscription was found for this user." });
       return;
     }
 
-    const subscriptionInfo = [];
+    const subscriptionData = [];
 
-    subscriptionInfo.push(
-      subscription.price,
-      subscription.type,
-      subscription.status,
-      subscription.start_date,
+    subscriptionData.push(subscription.description,
       subscription.end_date,
-      subscription.description
-    );
+      subscription.price,
+      subscription.start_date,
+      subscription.status,
+      subscription.type);
 
-    response(req, res, 200, subscriptionInfo);
+    response(req, res, 200, subscriptionData);
     return;
   } catch (err) {
     console.log(err);
-    response(req, res, 500, {
-      message: err
-    });
+    response(req, res, 500, { message: err });
     return;
   }
 }
@@ -55,16 +47,12 @@ const changeSubscription = async (req, res) => {
     const subscriptionPlan = req.body.subscriptionPlan;
 
     if (!userId) {
-      response(req, res, 400, {
-        error: "User ID parameter is missing in the URL."
-      });
+      response(req, res, 400, { error: "User ID parameter is missing in the URL." });
       return;
     }
 
     if (!subscriptionPlan) {
-      response(req, res, 400, {
-        error: "Please specify in the body the new subscription plan"
-      });
+      response(req, res, 400, { error: "Please specify in the body the new subscription plan" });
       return;
     }
 
@@ -81,16 +69,12 @@ const changeSubscription = async (req, res) => {
     });
 
     if (!subscription) {
-      response(req, res, 400, {
-        error: "No subscription found for this user"
-      });
+      response(req, res, 400, { error: "No subscription found for this user" });
       return;
     }
 
     if (subscription.type === subscriptionPlan) {
-      response(req, res, 400, {
-        error: "The user already has this subscription plan!"
-      });
+      response(req, res, 400, { error: "The user already has this subscription plan!" });
       return;
     }
 
@@ -106,9 +90,7 @@ const changeSubscription = async (req, res) => {
         plan = Plan.UHD;
         break;
       default:
-        response(req, res, 400, {
-          error: "Invalid subscription plan. Choose between SD, HD, UHD!"
-        });
+        response(req, res, 400, { error: "Invalid subscription plan. Choose between SD, HD, UHD!" });
         return;
     }
 
@@ -123,21 +105,15 @@ const changeSubscription = async (req, res) => {
     });
 
     if (!updatedSubscription) {
-      response(req, res, 400, {
-        error: "Subscription couldn't be updated, please try again!"
-      });
+      response(req, res, 400, { error: "Subscription couldn't be updated, please try again!" });
       return;
     }
 
-    response(req, res, 200, {
-      message: "Subscription updated succesfully!"
-    });
+    response(req, res, 200, { message: "Subscription updated succesfully!" });
     return;
   } catch (err) {
     console.log(err);
-    response(req, res, 500, {
-      error: "Internal server error"
-    });
+    response(req, res, 500, { error: "Internal server error" });
     return;
   }
 }
@@ -148,16 +124,12 @@ const renewSubscription = async (req, res) => {
     const subscriptionPlan = req.body.subscriptionPlan;
 
     if (!userId) {
-      response(req, res, 400, {
-        error: "User ID parameter is missing in the URL."
-      });
+      response(req, res, 400, { error: "User ID parameter is missing in the URL." });
       return;
     }
 
     if (!subscriptionPlan) {
-      response(req, res, 400, {
-        error: "Please specify in the body the subscription plan that you want to renew"
-      });
+      response(req, res, 400, { error: "Please specify in the body the subscription plan that you want to renew" });
       return;
     }
 
@@ -174,17 +146,13 @@ const renewSubscription = async (req, res) => {
     });
 
     if (!subscription) {
-      response(req, res, 403, {
-        error: "No subscription found for this user, please try again."
-      });
+      response(req, res, 403, { error: "No subscription found for this user, please try again." });
       return;
     }
 
     //compare end date with the current date
     if (new Date(subscription.end_date) < new Date().now) {
-      response(req, res, 400, {
-        error: "You can't renew your subscription yet! Wait until " + subscription.end_date
-      });
+      response(req, res, 400, { error: "You can't renew your subscription yet! Wait until " + subscription.end_date });
       return;
     }
 
@@ -200,9 +168,7 @@ const renewSubscription = async (req, res) => {
         plan = Plan.UHD;
         break;
       default:
-        response(req, res, 400, {
-          error: "Invalid subscription plan. Choose between SD, HD, UHD!"
-        });
+        response(req, res, 400, { error: "Invalid subscription plan. Choose between SD, HD, UHD!" });
         return;
     }
 
@@ -221,40 +187,20 @@ const renewSubscription = async (req, res) => {
       description: plan.description,
     });
 
-    response(req, res, 200, {
-      message: "Subscription renewed succesfully!"
-    });
+    response(req, res, 200, { message: "Subscription renewed succesfully!" });
     return;
   } catch (error) {
     console.log(error);
-    response(req, res, 500, {
-      error: "Internal server error"
-    });
+    response(req, res, 500, { error: "Internal server error" });
     return;
   }
 }
 
 //enum for subscription plans
 const Plan = Object.freeze({
-  SD: {
-    plan: "SD",
-    price: 7.99,
-    description: "This is the standard plan."
-  },
-  HD: {
-    plan: "HD",
-    price: 10.99,
-    description: "This is the medium plan."
-  },
-  UHD: {
-    plan: "UHD",
-    price: 13.99,
-    description: "This is the high plan."
-  }
+  SD: { plan: "SD", price: 7.99, description: "This is the standard plan." },
+  HD: { plan: "HD", price: 10.99, description: "This is the medium plan." },
+  UHD: { plan: "UHD", price: 13.99, description: "This is the high plan." }
 });
 
-module.exports = {
-  getSubscriptionInfo,
-  changeSubscription,
-  renewSubscription
-};
+module.exports = { getSubscriptionInfo, changeSubscription, renewSubscription };
